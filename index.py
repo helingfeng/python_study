1181c0adddce0c5ced4bd7004bd0de25f22f505a
helingfeng <857190327@qq.com>
2021-03-06 21:44:11 +0800

    feat: py quadratic

--- index.py
+++ index.py
@@ -71,3 +71,21 @@ def my_max(x, y):
 
 print(my_max(12, 9))
 print(my_max(8, 16))
+
+import math
+
+def quadratic(a, b, c):
+    n1 = -b + math.sqrt(b*b - 4*a*c)
+    n2 = -b - math.sqrt(b*b - 4*a*c)
+    m = 2*a
+    return n1/m , n2/m
+
+print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
+
+if quadratic(2, 3, 1) != (-0.5, -1.0):
+    print('测试失败')
+elif quadratic(1, 3, -4) != (1.0, -4.0):
+    print('测试失败')
+else:
+    print('测试成功')
