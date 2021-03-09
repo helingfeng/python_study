c9be5ff378b33527883458b8bf35c301a62d0ca0
helingfeng <857190327@qq.com>
2021-03-09 17:30:35 +0800

    docs: exception

--- index.py
+++ index.py
@@ -292,3 +292,24 @@ class Screen(object):
     @property
     def resolution(self):
         return self._width * self._height
+
+
+try:
+    print('try...')
+    r = 10 / int('a')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+finally:
+    print('finally...')
+print('END')
+
+try:
+    # foo()
+    pass
+except ValueError as e:
+    print('ValueError')
+except UnicodeError as e:
+    print('UnicodeError')
