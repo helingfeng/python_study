efe5def596984b0959556cf9627558e7e6be2893
helingfeng <857190327@qq.com>
2021-03-12 21:26:50 +0800

    test: param

--- index.py
+++ index.py
@@ -368,6 +368,7 @@ def to_timestamp(dt_str, tz_str):
     dt = datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
     return dt.replace(tzinfo=timezone(timedelta(hours=h))).timestamp()
 
+
 # 测试:
 t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
 assert t1 == 1433121030.0, t1
@@ -376,3 +377,12 @@ t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
 assert t2 == 1433121030.0, t2
 
 print('ok')
+
+
+def param_fun(a, b, c=None, **k):
+    print(a, b, c, k)
+
+
+param_fun(1, 2, c=3, d=4)
+param_fun(1, 2, 3)
+param_fun(1, 2, d=5, e=6)
